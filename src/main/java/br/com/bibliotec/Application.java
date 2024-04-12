17576fe3ce317ee15f5c559a3dc7faa2b406ee3b
André Gonçalves <andrevilanovagon@hotmail.com>
2024-04-12 00:04:21 -0300

    fix: MainView ok

--- src/main/java/br/com/bibliotec/Application.java
+++ src/main/java/br/com/bibliotec/Application.java
@@ -1,8 +1,5 @@
 package br.com.bibliotec;
 
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,8 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(variant = Lumo.DARK)
-public class Application implements AppShellConfigurator {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/br/com/bibliotec/ui/MainView.java
+++ src/main/java/br/com/bibliotec/ui/MainView.java
@@ -1,41 +1,98 @@
 package br.com.bibliotec.ui;
 
+import br.com.bibliotec.ui.book.BookGrid;
+import br.com.bibliotec.ui.bookloan.BookLoanGrid;
+import br.com.bibliotec.ui.student.StudentGrid;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.Scroller;
-import com.vaadin.flow.component.sidenav.SideNav;
-import com.vaadin.flow.component.sidenav.SideNavItem;
-import com.vaadin.flow.theme.lumo.LumoUtility;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
-public class MainView extends AppLayout {
+@Theme(value = "bibliotec", variant = Lumo.DARK)
+public class MainView extends AppLayout implements AppShellConfigurator, BeforeEnterObserver {
+    
+    private Div bookDiv;
+    private Div studentDiv;
+    private Div loanDiv;
+    private Div configDiv;
+    private Div startDiv;
     
     public MainView() {
+        VerticalLayout container = new VerticalLayout();
+        container.setClassName("header-container");
+        
+        HorizontalLayout tileContainer = new HorizontalLayout();
+        tileContainer.setClassName("title-container");
+        H4 title = new H4("BIBLIOTECA ESCOLA MUNICIPAL JOSÉ AUGUSTO GAMA DE SOUZA");
+        tileContainer.add(title);
+        
+        HorizontalLayout layoutButtons = createHeader();
+        
+        
+        container.add(tileContainer, layoutButtons);
+        addToNavbar(container);
+        
+    }
+    
+    private HorizontalLayout createHeader() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.addClassName("buttons-container");
 
-        DrawerToggle toggle = new DrawerToggle();
+        startDiv = new Div();
+        startDiv.setClassName("div-links-start");
+        RouterLink startLink = new RouterLink("INÍCIO", BookLoanGrid.class);
+        startDiv.add(startLink);
+        
+        HorizontalLayout endDiv = new HorizontalLayout();
+        endDiv.setClassName("div-links-container-end");
 
-        SideNav nav = getSideNav();
+        bookDiv = new Div();
+        bookDiv.setClassName("div-links-right");
+        RouterLink bookLink = new RouterLink("LIVROS", BookGrid.class);
+        bookDiv.add(bookLink);
+        endDiv.add(bookDiv);
 
-        Scroller scroller = new Scroller(nav);
-        scroller.addClassName("main-view-scroller-1");
-        scroller.setClassName(LumoUtility.Padding.SMALL);
+        studentDiv = new Div();
+        studentDiv.setClassName("div-links-right");
+        RouterLink studentLink = new RouterLink("ALUNOS", StudentGrid.class);
+        studentDiv.add(studentLink);
+        endDiv.add(studentDiv);
 
-        addToDrawer(scroller);
-        addToNavbar(toggle);
-        addClassName("main-view-app-layout-1");
+        loanDiv = new Div();
+        loanDiv.setClassName("div-links-right");
+        RouterLink loanLink = new RouterLink("EMPRÉSTIMOS", BookLoanGrid.class);
+        loanDiv.add(loanLink);
+        endDiv.add(loanDiv);
+
+        configDiv = new Div();
+        configDiv.setClassName("div-link-config");
+        RouterLink configLink = new RouterLink("CONFIGURAÇÕES", BookLoanGrid.class);
+        configDiv.add(configLink);
+        endDiv.add(configDiv);
+        
+        layout.add(startDiv, endDiv);
+        return layout;
     }
 
-    private SideNav getSideNav() {
-        SideNav sideNav = new SideNav();
-        sideNav.addItem(
-                new SideNavItem("Livro", "/livro",
-                        VaadinIcon.BOOK.create()),
-                new SideNavItem("Empréstimo", "/emprestimo",
-                        VaadinIcon.EXCHANGE.create()),
-                new SideNavItem("Aluno", "/aluno",
-                        VaadinIcon.NURSE.create())
-        );
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        String route = event.getLocation().getPath();
         
-        return sideNav;
+        bookDiv.getStyle().remove("background-color");
+        studentDiv.getStyle().remove("background-color");
+        loanDiv.getStyle().remove("background-color");
+        
+        switch (route) {
+            case "livro" -> bookDiv.getStyle().set("background-color", "var(--blue-color)");
+            case "aluno" -> studentDiv.getStyle().set("background-color", "var(--blue-color)");
+            case "emprestimo" -> loanDiv.getStyle().set("background-color", "var(--blue-color)");
+        }
     }
 }
